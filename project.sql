b3dd4d0033b2a18b07eb37883f46b0e7322f93b3
IPSSandhu <ishan3121@uvic.ca>
2024-06-09 14:26:05 -0700

    Data Pop

--- project.sql
+++ project.sql
@@ -1,69 +1,121 @@
--- Creates the Database with the name CompanyInventorydb
-CREATE DATABASE CompanyInventorydb;
-
--- Use the database created
-USE CompanyInventorydb;
-
--- Creating a table with the name Item
-CREATE TABLE Item(
-    item_id INT,
-    category VARCHAR(512),
-    quantity_sold INT,
-    item_name VARCHAR(512)
-);
-
--- Creating a table with the name Accounts
-CREATE TABLE Accounts(
-    total_revenue INT,
-    account_id INT,
+CREATE DATABASE `CompanyInventory`;
+USE `CompanyInventory`;
+DROP TABLE Accounts;
+CREATE TABLE `Accounts`(
+	`item_id` VARCHAR(512),
+    `total_revenue` INT,
+    `quantity_sold` INT
 ); 
-
--- Creating a table with the name SupplierOrder
-CREATE TABLE SupplierOrder(
-    order_id INT,
-    quantity INT,
-    unit_price INT,
-    total_price INT
+CREATE TABLE `SupplierOrder`(
+    `order_id` INT,
+    `quantity` INT,
+    `item_id` VARCHAR(512),
+    `unit_price` INT,
+    `total_price` INT
+);
+CREATE TABLE `Admin`(
+    `admin_id` INT,
+    `admin_role` VARCHAR(64)
+);
+CREATE TABLE `SaleInformation`(
+    `sale_id` INT,
+    `status` VARCHAR(64),
+    `order_items` VARCHAR(2048),
+    `date_sold` DATE,
+    `total_amound` INT,
+    `shipping_information` VARCHAR(1024)
+);
+CREATE TABLE `Warehouse`(
+    `address` VARCHAR(128),
+    `item_id` VARCHAR(512),
+    `item_total_quantity` INT,
+    `order_history` VARCHAR(2048)
+);
+CREATE TABLE `CustomerInformation`(
+    `customer_id` INT,
+    `customer_name` VARCHAR(512),
+    `email` VARCHAR(512),
+    `address` VARCHAR(128),
+    `phone_number` INT
+);
+CREATE TABLE `Supplier`(
+    `supplier_id` INT, 
+    `name` VARCHAR(512),
+    `money_owed` VARCHAR(512),
+    `order_history` VARCHAR(512),
+    `address` VARCHAR(128)
+);
+CREATE TABLE `Item`(
+    `item_id` INT,
+    `category` VARCHAR(128),
+    `item_name` VARCHAR(512),
+    `quantity_sold` INT
 );
 
--- Creating a table with the name Admin
-CREATE TABLE Admin(
-    admin_role VARCHAR(64),
-    admin_id INT
-);
+SHOW VARIABLES LIKE 'secure_file_priv';
 
--- Creating a table with the name SaleInformation
-CREATE TABLE SaleInformation(
-    sale_id INT,
-    status_order VARCHAR(64),
-    order_items VARCHAR(2048),
-    date_sold DATE,
-    total_amound INT,
-    shipping_information VARCHAR(1024)
-);
 
--- Creating a table with the name Warehouse
-CREATE TABLE Warehouse(
-    address_warehouse VARCHAR(128),
-    item_total_quantity INT,
-    order_history VARCHAR(2048)
-);
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\accounts_data.csv'
+INTO TABLE Accounts
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
--- Creating a table with the name ContactInformation
-CREATE TABLE CustomerInformation(
-    customer_id INT,
-    customer_name VARCHAR(512),
-    email VARCHAR(512),
-    address VARCHAR(128),
-    phone_number INT
-);
+DESCRIBE Accounts;
+SELECT * FROM Accounts;
 
--- Creating a table with the name Supplier
-CREATE TABLE Supplier(
-    supplier_id INT,
-    item_name VARCHAR(512), 
-    name VARCHAR(512),
-    money_owed VARCHAR(512),
-    order_history VARCHAR(512)
-);
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\admin_data.csv'
+INTO TABLE Admin
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
+SELECT * FROM Admin;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\customer_information_data.csv'
+INTO TABLE CustomerInformation
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+SELECT * FROM CustomerInformation;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\item_data.csv'
+INTO TABLE Item
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+SELECT * FROM Item;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\sale_information_data.csv'
+INTO TABLE SaleInformation
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\supplier_data.csv'
+INTO TABLE Supplier
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\supplier_order_data.csv'
+INTO TABLE SupplierOrder
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\warehouse_data.csv'
+INTO TABLE Warehouse
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
