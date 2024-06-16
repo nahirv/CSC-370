9b72ce6fa93075166c9d2fefe85cea8d5cd48df5
Nagarjun2519 <nagarjun2519@gmail.com>
2024-06-16 00:21:25 -0700

    Update project.sql
    Added primary and foreign keys

--- project.sql
+++ project.sql
@@ -1,59 +1,71 @@
 CREATE DATABASE `CompanyInventory`;
 USE `CompanyInventory`;
-DROP TABLE Accounts;
+
+-- Creating the Tables
 CREATE TABLE `Accounts`(
-	`item_id` VARCHAR(512),
+	`item_id` INT PRIMARY KEY,
     `total_revenue` INT,
     `quantity_sold` INT
 ); 
 CREATE TABLE `SupplierOrder`(
-    `order_id` INT,
+    `order_id` INT PRIMARY KEY, 
     `quantity` INT,
-    `item_id` VARCHAR(512),
+    `item_id` INT, 
     `unit_price` INT,
     `total_price` INT
 );
 CREATE TABLE `Admin`(
-    `admin_id` INT,
+    `admin_id` INT PRIMARY KEY,
     `admin_role` VARCHAR(64)
 );
 CREATE TABLE `SaleInformation`(
-    `sale_id` INT,
+    `sale_id` INT PRIMARY KEY,
     `status` VARCHAR(64),
     `order_items` VARCHAR(2048),
     `date_sold` DATE,
-    `total_amound` INT,
+    `total_amount` INT,
     `shipping_information` VARCHAR(1024)
 );
 CREATE TABLE `Warehouse`(
-    `address` VARCHAR(128),
-    `item_id` VARCHAR(512),
+    `address` VARCHAR(128) PRIMARY KEY,
+    `item_id` INT, 
     `item_total_quantity` INT,
     `order_history` VARCHAR(2048)
 );
 CREATE TABLE `CustomerInformation`(
-    `customer_id` INT,
+    `customer_id` INT PRIMARY KEY, 
     `customer_name` VARCHAR(512),
     `email` VARCHAR(512),
     `address` VARCHAR(128),
     `phone_number` INT
 );
 CREATE TABLE `Supplier`(
-    `supplier_id` INT, 
+    `supplier_id` INT PRIMARY KEY, 
     `name` VARCHAR(512),
     `money_owed` VARCHAR(512),
     `order_history` VARCHAR(512),
     `address` VARCHAR(128)
 );
 CREATE TABLE `Item`(
-    `item_id` INT,
+    `item_id` INT PRIMARY KEY, 
     `category` VARCHAR(128),
     `item_name` VARCHAR(512),
     `quantity_sold` INT
 );
 
-SHOW VARIABLES LIKE 'secure_file_priv';
+-- Adding Foreign Keys for Accounts, Supplier and Warehouse
+ALTER TABLE `Accounts`
+ADD CONSTRAINT `fk_accounts_item_id`
+FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
+ALTER TABLE `SupplierOrder`
+ADD CONSTRAINT `fk_supplier+order_item_id`
+FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
+ALTER TABLE `Warehouse`
+ADD CONSTRAINT `fk_warehouse_item_id`
+FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
 
+-- Data Propogation
+SHOW VARIABLES LIKE 'secure_file_priv';
 
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\accounts_data.csv'
 INTO TABLE Accounts
