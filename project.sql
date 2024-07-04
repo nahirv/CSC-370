8331c04eeda958fcac5fa16feb4c120aaf76feee
Olivia Choi <135431556+oliviachoiii@users.noreply.github.com>
2024-07-03 18:38:20 -0700

    Update project.sql

--- project.sql
+++ project.sql
@@ -2,67 +2,87 @@ CREATE DATABASE `CompanyInventory`;
 USE `CompanyInventory`;
 
 -- Creating the Tables
-CREATE TABLE `Accounts`(
-	`item_id` INT PRIMARY KEY,
-    `total_revenue` INT,
-    `quantity_sold` INT
-); 
-CREATE TABLE `SupplierOrder`(
-    `order_id` INT PRIMARY KEY, 
-    `quantity` INT,
-    `item_id` INT, 
-    `unit_price` INT,
-    `total_price` INT
-);
-CREATE TABLE `Admin`(
-    `admin_id` INT PRIMARY KEY,
-    `admin_role` VARCHAR(64)
-);
-CREATE TABLE `SaleInformation`(
-    `sale_id` INT PRIMARY KEY,
-    `status` VARCHAR(64),
-    `order_items` VARCHAR(2048),
-    `date_sold` DATE,
-    `total_amount` INT,
-    `shipping_information` VARCHAR(1024)
-);
-CREATE TABLE `Warehouse`(
-    `address` VARCHAR(128) PRIMARY KEY,
-    `item_id` INT, 
-    `item_total_quantity` INT,
-    `order_history` VARCHAR(2048)
-);
-CREATE TABLE `CustomerInformation`(
-    `customer_id` INT PRIMARY KEY, 
-    `customer_name` VARCHAR(512),
-    `email` VARCHAR(512),
-    `address` VARCHAR(128),
-    `phone_number` INT
-);
-CREATE TABLE `Supplier`(
-    `supplier_id` INT PRIMARY KEY, 
-    `name` VARCHAR(512),
-    `money_owed` VARCHAR(512),
-    `order_history` VARCHAR(512),
-    `address` VARCHAR(128)
-);
-CREATE TABLE `Item`(
-    `item_id` INT PRIMARY KEY, 
-    `category` VARCHAR(128),
+
+-- Item (Base Class)
+CREATE TABLE `Item` (
+    `item_id` INT PRIMARY KEY,
+    `category` VARCHAR(512),
     `item_name` VARCHAR(512),
     `quantity_sold` INT
 );
 
--- Adding Foreign Keys for Accounts, Supplier and Warehouse
-ALTER TABLE `Accounts`
-ADD CONSTRAINT `fk_accounts_item_id`
-FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
-ALTER TABLE `SupplierOrder`
-ADD CONSTRAINT `fk_supplier+order_item_id`
-FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
-ALTER TABLE `Warehouse`
-ADD CONSTRAINT `fk_warehouse_item_id`
-FOREIGN KEY (`item_id`) REFERENCES `Item`(`item_id`);
+-- Accounts (Subclass)
+CREATE TABLE `Accounts`(
+	`item_id` INT PRIMARY KEY,
+    `sale_unit_price` INT,
+    `total_revenue` INT,
+    `quantity_sold` INT,
+    FOREIGN KEY (`item_id`) REFERENCES Item(`item_id`)
+); 
+
+-- Warehouse (Subclass)
+CREATE TABLE `Warehouse`(
+    `address` VARCHAR(128) PRIMARY KEY,
+    `item_id` INT, 
+    `item_total_quantity` INT,
+    `order_history` VARCHAR(2048),
+    FOREIGN KEY (`item_id`) REFERENCES Item(`item_id`)
+);
+
+-- Supplier Information (Base Class)
+CREATE TABLE `SupplierInformation`(
+    `address` VARCHAR(512) PRIMARY KEY,
+    `name` VARCHAR(512),
+    `money_owed` VARCHAR(512),
+    `order_history` VARCHAR(512)
+);
+
+-- Supplier Order (Subclass)
+CREATE TABLE `SupplierOrder`(
+    `order_id` INT PRIMARY KEY,
+    `address` VARCHAR(512),
+    `item_id` INT, 
+    `quantity` INT,
+    `unit_price` INT,
+    `total_price` INT,
+    `order_date` DATE,
+    FOREIGN KEY (`address`) REFERENCES SupplierInformation(`address`),
+    FOREIGN KEY (`item_id`) REFERENCES Item(`item_id`)
+);
+
+-- Admin
+CREATE TABLE `Admin`(
+    `admin_id` INT PRIMARY KEY,
+    `admin_role` VARCHAR(64)
+);
+
+-- Sale Information
+CREATE TABLE `SaleInformation`(
+    `sale_id` INT PRIMARY KEY,
+    `status` VARCHAR(64),
+    `customer_id` INT,
+    `sale_items` VARCHAR(2048),
+    `date_sold` DATE,
+    `total_amount` INT,
+    `shipping_information` VARCHAR(1024),
+    FOREIGN KEY (`customer_id`) REFERENCES CustomerInformation(`customer_id`)
+);
+
+-- Customer Information
+CREATE TABLE `CustomerInformation`(
+    `customer_id` INT PRIMARY KEY, 
+    `email` VARCHAR(512),
+    `phone_number` INT,
+    `address` VARCHAR(128)
+);
+
+-- Customer Names (Weak Entity Set)
+CREATE TABLE `CustomerNames` (
+    `customer_id` INT,
+    `customer_name` VARCHAR(64),
+    PRIMARY KEY (`customer_id`, `customer_name`),
+    FOREIGN KEY (`customer_id`) REFERENCES CustomerInformation(`customer_id`)
+);
 
 -- Data Propogation
 SHOW VARIABLES LIKE 'secure_file_priv';
@@ -100,6 +120,13 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\customer_names_data.csv'
+INTO TABLE CustomerNames
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
 SELECT * FROM CustomerInformation;
 
 SELECT * FROM Item;
@@ -112,7 +139,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\database_sample_data\\supplier_data.csv'
-INTO TABLE Supplier
+INTO TABLE SupplierInformation
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
